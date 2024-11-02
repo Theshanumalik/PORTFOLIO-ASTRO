55f15cecd3325f5d43f0051c522e5f84e3c9ae82
theshanumalik <shanumalik9001567@gmail.com>
2024-11-02 17:40:24 +0530

    removed vercle analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  output: "server",
   integrations: [tailwind(), icon(), mdx(), react()],
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
