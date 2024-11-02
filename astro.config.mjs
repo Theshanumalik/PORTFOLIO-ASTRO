e148f052fc221af02d2456eb967253dc8db4cf38
theshanumalik <shanumalik9001567@gmail.com>
2024-11-02 17:58:24 +0530

    add @astro/vercel which was accidently removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercelServerless(),
   integrations: [tailwind(), icon(), mdx(), react()],
 });
